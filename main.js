c23a15684fbe64727f8adcd5dd3d6d3133cc9535
amir-38 <a_saruxanov_38@mail.ru>
2023-12-12 19:09:50 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+VANTA.CELLS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  color1: 0x10f0f,
+  color2: 0x708af,
+  size: 1.2,
+});
